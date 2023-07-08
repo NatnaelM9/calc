9147661a52f6aee7a20064156df73ab42b993500
natnaelm97 <natnaelmehari97@gmail.com>
2023-07-08 16:39:14 +0300

    Add calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * add - add two numbers
+ * x: number
+ * y: number
+ *
+ * Return: sum of x and y
+ */
+int add(int x, int y);
+
+/**
+ * add - subtract two numbers
+ * x: number
+ * y: number
+ *
+ * Return: difference of x and y
+ */
+int sub(int x, int y);
+
+/**
+ * add - multiply two numbers
+ * x: number
+ * y: number
+ *
+ * Return: product of x and y
+ */
+int mul(int x, int y);
+
+/**
+ * add - divide two numbers
+ * x: number
+ * y: number
+ *
+ * Return: quotient of x and y
+ */
+int div(int x, int y);
+
+int main(void)
+{
+	
+	return (0);
+}
+
+int add(int x, int y);
+
+int sub(int x, int y);
+
+int mul(int x, int y);
+
+int div(int x, int y);
