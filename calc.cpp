884c6f47b38c316106bed0359f36beb51ec81d4c
natnaelm97 <natnaelmehari97@gmail.com>
2023-07-08 16:51:10 +0300

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -2,42 +2,18 @@
 
 using namespace std;
 
-/**
- * add - add two numbers
- * x: number
- * y: number
- *
- * Return: sum of x and y
- */
 int add(int x, int y){
 	return x + y;
 }
 
-
- 
- 
 int sub(int x, int y){
 	return x - y;
 }
 
-/**
- * add - multiply two numbers
- * x: number
- * y: number
- *
- * Return: product of x and y
- */
 int mul(int x, int y){
 	return x * y;
 }
 
-/**
- * add - divide two numbers
- * x: number
- * y: number
- *
- * Return: quotient of x and y
- */
 int div(int x, int y){
 	return x / y;
 }
@@ -47,11 +23,3 @@ int main(void)
 	
 	return (0);
 }
-
-int add(int x, int y);
-
-int sub(int x, int y);
-
-int mul(int x, int y);
-
-int div(int x, int y);
